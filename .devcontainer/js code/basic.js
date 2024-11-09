cefc4f4eb69f59fa7d2d6a4f1609e583678dd420
babar646 <babar8092@gmail.com>
2024-11-09 05:44:08 +0000

    conversion of data type

---
+++ .devcontainer/js code/basic.js
@@ -0,0 +1,12 @@
+let score =undefined
+console.log(typeof score);
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber) ;
+console.log(valueinNumber);
+let IsLoggedIn = "babar"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
+let someNumber = 33
+let stringnumber =String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
