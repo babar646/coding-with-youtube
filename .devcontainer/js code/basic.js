74acc2128ee9a3ed2254b9bad3324184113a5676
babar646 <babar8092@gmail.com>
2024-12-08 13:24:40 +0000

    basic.commit

--- .devcontainer/js code/basic.js
+++ .devcontainer/js code/basic.js
@@ -1,12 +1,17 @@
-let score =undefined
-console.log(typeof score);
-let valueinNumber = Number(score);
-console.log(typeof valueinNumber) ;
-console.log(valueinNumber);
-let IsLoggedIn = "babar"
-let booleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(booleanIsLoggedIn)
-let someNumber = 33
-let stringnumber =String(someNumber)
-console.log(someNumber);
-console.log(typeof someNumber);
+// let score =undefined
+// console.log(typeof score);
+// let valueinNumber = Number(score);
+// console.log(typeof valueinNumber) ;
+// console.log(valueinNumber);
+// let IsLoggedIn = "babar"
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// let someNumber = 33
+// let stringnumber =String(someNumber)
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+// let num1 = 13
+// let num2 = 2
+// let num3 =  num1 ** num2
+// console.log(num3);
